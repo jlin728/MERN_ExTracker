ca1a4e6a83246a93b66ddc0a9668ba9e020932d7
jihfan <jiffcodes@gmail.com>
2019-07-10 22:43:35 -0600

    Ex Methods added

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
 export default class CreateExercise extends Component {
+    
+    
+    constructor(props){
+    {/* @1:01:32*/}
+        super(props);
+        this.state = {
+            username: '',
+            description: '',
+            duration: 0,
+            date:   new Date(),
+            users: []
+        }
+    }
+    
+    onChangeUserName(e){
+    {/*methods*/}
+        this.setState({
+            username: e.target.value
+            {/*target is the text box in the form, value is value*/}
+        });
+    }
+
+
+    
+    
     render (){
         return (
             <div>
                 <p>Create New Exes Here (Create-Exercise List Component)</p>
-                {/* @1:01:32*/}
+                
             </div>
         )
     }
